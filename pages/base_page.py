32eae8c19456d8dc63666fd3fb9d3f0ddba59222
MariaSw <kryvunmaria@gmail.com>
2020-03-04 22:26:46 +0300

    test should see login link added with assert

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,10 +1,14 @@
 #базовая страница, от которой будут унаследованы все остальные классы.
 # В ней мы опишем вспомогательные методы для работы с драйвером
+
+from selenium.common.exceptions import NoSuchElementException
+
 class BasePage():
     #конструктор — метод, который вызывается, когда мы создаем объект
-    def __init__(self, browser, url):
+    def __init__(self, browser, url, timeout=10):
         self.browser = browser
         self.url = url
+        self.browser.implicitly_wait(timeout)
 
     #открывать нужную страницу в браузере
     def open(self):
@@ -12,3 +16,11 @@ class BasePage():
         self.browser.get(self.url)
         #за пределами класса это будет вызываться так
         #BasePage.browser.get(BasePage.url)
+
+
+    def is_element_present(self, how, what):
+        try:
+            self.browser.find_element(how, what)
+        except NoSuchElementException:
+            return False
+        return True
